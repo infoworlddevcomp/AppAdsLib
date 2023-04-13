4666f79f3397825ec04da47d3b3f61a463bdb5d3
admin <nikunjbutani.yami@gmail.com>
2023-04-13 17:50:10 +0530

    bug fixed

--- appadslibrary/src/main/java/com/apps/games/appadslibrary/AppInfo.java
+++ appadslibrary/src/main/java/com/apps/games/appadslibrary/AppInfo.java
@@ -6,7 +6,7 @@ import android.graphics.drawable.Drawable;
  * Created by enyason on 7/29/18.
  */
 
-class AppInfo {
+public class AppInfo {
 
     CharSequence label;
     CharSequence packageName;
